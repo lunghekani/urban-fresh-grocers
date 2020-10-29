617743f48d6edca0851e91a7e7cadb938a743a08
Lunghekani <lunghivlanga@gmail.com>
2020-10-29 19:35:13 +0200

    added about the app details

--- app/screens/AboutScreen.js
+++ app/screens/AboutScreen.js
@@ -4,7 +4,8 @@ import Text from '../components/Text'
 function AboutScreen(props) {
   return (
     <View style={styles.container}>
-        <Text  >About the app</Text>
+        <Text  >The app provides food items on special from varies stores. The user is able to search the store that they would like to view items on.</Text>
+        <Text  >The app makes it easier for users to find specials in digital form thus making it environmentally friendly.</Text>
     </View>
   );
 }
